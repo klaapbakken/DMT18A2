e9a69d40caf09e3a68800488cb56f919f83b5aa2
Øyvind Klåpbakken <oyvind.klaapbakken@gmail.com>
2018-05-18 15:42:33 +0200

    Added comments

--- evaluate.R
+++ evaluate.R
@@ -2,7 +2,8 @@ source("ndcg.R")
 library(foreach)
 library(iterators)
 
-query_extraction <- function(df, query){
+query_evaluation <- function(df, query){
+  #Extracting relevant columns from df, passing them to nDCG()
   query_i <- which(df$srch_id == query)
   query_df <- df[query_i, c("prop_id", "relevance",
                              "predicted_relevance")]
@@ -12,14 +13,12 @@ query_extraction <- function(df, query){
 }
 
 evaluate <- function(df){
-  #All numeric columns for relevant columns in df
+  #Columns of df should be as.numeric for "srch_id" and "prop_ids".
+  #Getting unique queries
   queries <- iter(unique(df$srch_id))
+  #Calculating nDCG for each query
   ndcg_vector <- foreach(query=queries, .combine='c', .inorder=FALSE) %do% {
-    query_extraction(df, query)
+    query_evaluation(df, query)
   }
   return(sum(ndcg_vector)/length(unique(df$srch_id)))
 }
-
-test_df$srch_id <- as.numeric(test_df$srch_id)
-test_df$prop_id <- as.numeric(test_df$prop_id)
-evaluate(test_df)
