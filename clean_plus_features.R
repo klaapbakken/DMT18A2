4af7a8f0e57fc970e48caffdf75ace08b245e078
Nicola Ruffino <n.ruffino@student.vu.nl>
2018-05-25 02:50:52 +0200

    correct if test

--- clean_plus_features.R
+++ clean_plus_features.R
@@ -22,7 +22,7 @@ create_family_features <- function(expedia_tab){
   expedia_tab <- expedia_tab %>%
     mutate(family_factor = (case_when(srch_adults_count==0 & srch_children_count>0~-2,srch_adults_count==0~-1,
                                       srch_adults_count<=1 & srch_adults_count>0 & srch_children_count<1 ~0,
-                                      srch_adults_count<=1 & srch_children_count>0 ~1,
+                                      srch_adults_count==1 & srch_children_count>0 ~1,
                                       srch_adults_count>1 & srch_children_count<1 ~2, 
                                       srch_adults_count>0 & srch_children_count>0 & srch_children_count<2 ~ 3,
                                       srch_adults_count>0 & srch_children_count>1  ~ 4
