ea649a198f3abc1a277c18daaf29f039224c7657
Øyvind Klåpbakken <oyvind.klaapbakken@gmail.com>
2018-05-26 13:42:47 +0200

    Changed parameter name within function. Added functions for book frequency.

--- popularity.R
+++ popularity.R
@@ -12,7 +12,25 @@ extract_popularity <- function(train_df){
 }
 
 #Popularity of property is added to the data frame
-apply_popularity <- function(df, pop_df){
+apply_popularity <- function(test_df, pop_df){
   new_df <- left_join(test_df, pop_df, by = "prop_id") %>% replace(., is.na(.), 0)
+  return(new_df)
+}
+
+extract_week_book_frequency <- function(train_df){
+  responses <- group_by(train_df, week) %>% 
+    summarise(total_queries = n(),
+              total_bookings = sum(booking_bool),
+              total_clicks = sum(click_bool))
+  book_frequency <- group_by(responses, week) %>%
+    summarise(book_frequency = total_bookings/total_queries) %>%
+    as.data.frame
+  return(book_frequency)
+}
+
+apply_week_book_frequency <- function(test_df, book_freq_df){
+  mean_book_freq <- mean(book_freq_df$book_freq)
+  new_df <- left_join(test_df, book_freq_df, by = "week") %>%
+    replace(., is.na(.), mean_book_freq)
   return(new_df)
 }
